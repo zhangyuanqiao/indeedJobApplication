9ca9c1ad5d0f161aa4ebaa024007a0a9a3144dad
peidong Hu <zhouyibhic@gmail.com>
2015-07-02 01:16:40 -0400

    inital version

---
+++ com.bigtester.atescripts.indeedusfengfei/src/test/java/com/bigtester/AppTest.java
@@ -0,0 +1,36 @@
+package com.bigtester;
+
+import org.testng.annotations.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+   
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+	@Test (groups={"functest"})
+    public AppTest( String testName )
+    {
+//		try {
+//			TestProjectRunner.main("indeedJobApplication/testproject.xml");
+//		} catch (ClassNotFoundException e) {
+//			Assert.assertTrue(false);
+//		} catch (DatabaseUnitException e) {
+//			Assert.assertTrue(false);
+//		} catch (SQLException e) {
+//			Assert.assertTrue(false);
+//		} catch (IOException e) {
+//			Assert.assertTrue(false);
+//		} catch (ParseException e) {
+//			Assert.assertTrue(false);
+//		}
+    }
+
+  
+}
