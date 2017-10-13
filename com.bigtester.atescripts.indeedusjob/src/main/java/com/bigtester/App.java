0e5d4b9f79d48590a524ab9f45dc7d130f7958db
peidong <zhouyibhic@gmail.com>
2017-10-13 10:52:03 +0000

    app for debug

--- com.bigtester.atescripts.indeedusjob/src/main/java/com/bigtester/App.java
+++ com.bigtester.atescripts.indeedusjob/src/main/java/com/bigtester/App.java
@@ -1,5 +1,13 @@
 package com.bigtester;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.bigtester.ate.TestProjectRunner;
+import org.dbunit.DatabaseUnitException;
+
+import com.github.javaparser.ParseException;
+
 /**
  * Hello world!
  *
@@ -8,6 +16,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	try {
+			TestProjectRunner.runTest("indeedJobApplication/testproject.xml");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (DatabaseUnitException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
