bb547c8208f1ea50648284fdf228d6b2f4692759
peidong <zhouyibhic@gmail.com>
2018-08-09 04:08:54 +0000

    fengfei

--- com.bigtester.atescripts.indeedusjob/src/main/java/com/bigtester/casestep/SaveAppliedJobReference.java
+++ com.bigtester.atescripts.indeedusjob/src/main/java/com/bigtester/casestep/SaveAppliedJobReference.java
@@ -55,7 +55,8 @@ public class SaveAppliedJobReference extends AbstractBaseJavaCodedStep
 	 */
 	public void doStep(@Nullable IStepJumpingEnclosedContainer jumpingContainer) throws StepExecutionException,
 			PageValidationException, RuntimeDataException {
-		WebElement applyButtonLink = getMyWebDriver().getWebDriverInstance().findElement(By.xpath("(//span[@class='indeed-apply-widget indeed-apply-button-container indeed-apply-status-not-applied'])[1]"));
+//		WebElement applyButtonLink = getMyWebDriver().getWebDriverInstance().findElement(By.xpath("(//span[@class='indeed-apply-widget indeed-apply-button-container indeed-apply-status-not-applied'])[0]"));
+		WebElement applyButtonLink = getMyWebDriver().getWebDriverInstance().findElement(By.xpath("(//span[@data-indeed-apply-jobid])[1]"));
 		String jobApplyID = applyButtonLink.getAttribute("data-indeed-apply-jobid");
 		try {
 			File yourFile = new File(JOBREFERENCESSAVEFILE);
